6ca4493642839769d285d504746516022a704dcc
Jason Talon <jason.talon@radix.com.au>
2020-12-27 20:19:24 +0800

    test pwa

--- plugins/beforeinstallprompt.client.js
+++ plugins/beforeinstallprompt.client.js
@@ -6,6 +6,6 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Stash the event so it can be triggered later.
   //deferredPrompt = e
   Vue.prototype.$addToHome = e
-  alert("can add to home screen!")
+  //alert("can add to home screen!")
   // Update UI notify the user they can install the PWA
 })
