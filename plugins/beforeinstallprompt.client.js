559cf0a2810e7d5f26ba8dd1a185a7d16ef1021e
Jason Talon <jason.talon@radix.com.au>
2020-12-27 19:58:02 +0800

    test install prompt

--- plugins/beforeinstallprompt.client.js
+++ plugins/beforeinstallprompt.client.js
@@ -6,6 +6,6 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Stash the event so it can be triggered later.
   deferredPrompt = e
   // Update UI notify the user they can install the PWA
-  showInstallPromotion()
-  console.log('hello')
+
+  alert('hello')
 })
