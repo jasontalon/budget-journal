a9fdea6c435359bbd70fbff98742e61d64e5108d
Jason Talon <jason.talon@radix.com.au>
2020-12-27 20:12:22 +0800

    test pwa install

--- plugins/beforeinstallprompt.client.js
+++ plugins/beforeinstallprompt.client.js
@@ -1,11 +1,11 @@
-let deferredPrompt
+import Vue from 'vue'
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the mini-infobar from appearing on mobile
   e.preventDefault()
   // Stash the event so it can be triggered later.
-  deferredPrompt = e
+  //deferredPrompt = e
+  Vue.prototype.$addToHome = e
+  alert("can add to home screen!")
   // Update UI notify the user they can install the PWA
-
-  alert('hello')
 })
