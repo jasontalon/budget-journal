93cb23ade5a5257a964ea3439d28f9183230fa75
Jason Talon <jason.talon@radix.com.au>
2020-12-27 21:04:56 +0800

    expense store

---
+++ store/expense.js
@@ -0,0 +1,30 @@
+import dayjs from 'dayjs'
+import 'dayjs/plugin/utc'
+const EXPENSES_KEY = '__EXPENSES'
+
+export const state = () => ({
+  expenses: localStorage.getItem(EXPENSES_KEY) ?? [],
+})
+
+export const mutations = {
+  add(state, { id, category, description, amount }) {
+    state.expenses.add({
+      id,
+      category,
+      description,
+      amount,
+      createdAt: dayjs().utc(),
+    })
+  },
+  remove(state, id) {
+    const updatedExpenses = state.expenses.filter((p) => p.id !== id)
+    state.expenses = updatedExpenses
+  },
+  update(state, { id, category, description, amount }) {
+    state.expenses = state.expenses.map((p) => {
+      p.id === id
+        ? { ...p, category, description, amount, updatedAt: dayjs().utc() }
+        : p
+    })
+  },
+}
