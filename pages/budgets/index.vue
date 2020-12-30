cd2d3230ddd32302f1ab31932c171d9fd0d4ab6e
Jason Talon <jason.talon@outlook.com>
2020-12-30 10:32:25 +0000

    remove budget

--- pages/budgets/index.vue
+++ pages/budgets/index.vue
@@ -7,8 +7,25 @@
         v-for="budget in this.$store.state.budget.budgets"
         :key="budget.id"
       >
-        <div class="font-semibold text-gray-600 text-sm uppercase">
-          {{ budget.name }}
+        <div class="flex flex-row justify-between">
+          <div class="font-semibold text-gray-600 text-sm uppercase">
+            {{ budget.name }}
+          </div>
+          <div class="h-4 w-4 text-red-400" v-on:click="remove(budget.id)">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              />
+            </svg>
+          </div>
         </div>
         <div class="bg-green-200 py-4 px-2 rounded-md">
           <div class="flex flex-row justify-between">
@@ -17,14 +34,14 @@
               <div>Spent</div>
             </div>
             <div class="w-full text-right">
-              <div>{{ budget.amount }}</div>
+              <div class="font-semibold">{{ budget.amount }}</div>
               <div>Goal</div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <add-button :link='{path: "/budgets/new"}' />
+    <add-button :link="{ path: '/budgets/new' }" />
   </div>
 </template>
 
@@ -35,6 +52,11 @@ export default {
   components: { HeaderBar, AddButton },
   mounted() {
     console.log(this.$store.state)
+  },
+  methods: {
+    remove(budgetId) {
+      this.$store.commit('budget/remove', budgetId)
+    },
   },
 }
 </script>
